75ff430007dbb8d1795853f781856b96341c1667
AshokKadubandi <velmaashok765@gmail.com>
2025-05-21 16:40:02 +0530

    updated v-5.0.1

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ function generateId(){
     return id
 }
 
-let todoList = []
+function getFromLocalStorage(){
+    const storage = localStorage.getItem('todo')
+    if(storage){
+        return JSON.parse(storage)
+    }else{
+        return []
+    }
+}
+
+
+let todoList = getFromLocalStorage()
+console.log(todoList)
 let formSub = document.getElementById('formSub')
 let taskName = document.getElementById('task-title')
 let taskPrior = document.getElementById('prior')
 let taskTimeDate = document.getElementById('time-date')
 let todoTaskCon = document.getElementById('todo-task-cards')
+let todoSvaBtn = document.getElementById('saveData')
+
+todoSvaBtn.onclick = function(){
+    console.log(todoList)
+    localStorage.setItem("todo",JSON.stringify(todoList));
+
+}
+
 
 console.log(taskPrior.value)
 
@@ -19,7 +38,7 @@ let todoObj = initialTodo
 
 function craeteTodoContainerView(todo){
     console.log(todo)
-    const {taskName,priority,date,checked,id} = todo
+    const {taskName,priority,date,check,id,notes} = todo
     const todoCard = document.createElement('div');
     const checkBox = document.createElement('input')
     const todoContent = document.createElement('div');
@@ -28,14 +47,18 @@ function craeteTodoContainerView(todo){
     const todoContentPrior = document.createElement('p');
     const todoContentDate = document.createElement('p')
     const todoEditContainer = document.createElement('div');
-    const todoEditText = document.createElement('p')
-    const todoDeleteText = document.createElement('p')
+    const todoEditText = document.createElement('button')
+    const todoDeleteText = document.createElement('button')
+
+    const todoNotesCon = document.createElement('div')
+    const todoNotesTextArea = document.createElement('textarea')
+    todoNotesCon.appendChild(todoNotesTextArea);
 
     todoCard.classList.add('todo-card')
 
 
     const checkCheckBox = todoid => {
-        todoList.map(each => {
+        todoList.forEach(each => {
             if(each.id === todoid){
                 each.check = !each.check
             }
@@ -48,8 +71,9 @@ function craeteTodoContainerView(todo){
 
     checkBox.id = id
     checkBox.type = 'checkbox'
-    checkBox.classList.add('check-box')
-    checkBox.checked = checked
+    checkBox.style.backgroundColor = 'red'
+    checkBox.setAttribute('class','styled-checkbox')
+    checkBox.checked = check
     checkBox.addEventListener('click',function(){
         checkCheckBox(id)
     })
@@ -85,10 +109,38 @@ function craeteTodoContainerView(todo){
     todoConfig.appendChild(todoContentDate)
 
     todoEditText.textContent = 'Edit'
+    todoEditText.classList.add('edit-btn')
+    todoEditText.onclick = function(){
+        notes.status = !notes.status
+        todoTaskCon.textContent = ''
+        createTodoView()
+    }
+
     todoDeleteText.textContent = 'Del'
+    todoDeleteText.classList.add('del-btn')
+
+    todoEditContainer.classList.add('todo-edit-container')
 
     todoEditContainer.appendChild(todoEditText)
     todoEditContainer.appendChild(todoDeleteText)
+
+    todoNotesTextArea.addEventListener('keyup',function(e){
+        if(e.key === 'Enter'){
+            console.log(todoNotesTextArea.value)
+            todo.taskName = todoNotesTextArea.value
+            notes.status = !notes.status
+            todoTaskCon.textContent = ''
+            createTodoView()
+
+        }
+    })
+
+    if(notes.status){
+        todoCard.appendChild(todoNotesCon)
+    }else{
+        
+        // todoCard.removeChild(todoNotesCon)
+    }
 
 }
 
@@ -100,21 +152,35 @@ function createTodoView(){
     }
 }
 
-
+function checkValidations(){
+    if(taskName.value === '' || taskPrior.value === '' || taskTimeDate.value === ''){
+        alert('Please Enter all details of the todo')
+        return false
+    }
+    return true
+}
 
 formSub.addEventListener('submit',function(e){
     e.preventDefault()
-    const newTodoObj = {
-        id:generateId(),
-        taskName:taskName.value,
-        priority:taskPrior.value,
-        date:taskTimeDate.value,
-        check:false
+    if(checkValidations()){
+        const newTodoObj = {
+            id:generateId(),
+            taskName:taskName.value,
+            priority:taskPrior.value,
+            date:taskTimeDate.value,
+            check:false,
+            notes:{
+                state:false,
+                content:''
+            }
+        }
+        todoList.push(newTodoObj)
+        taskName.value = ''
+        taskPrior.value = 'Low'
+        taskTimeDate.value = ''
+        todoTaskCon.textContent = ''
+        createTodoView()
     }
-    todoList.push(newTodoObj)
-    taskName.value = ''
-    taskPrior.value = ''
-    taskTimeDate.value = ''
-    todoTaskCon.textContent = ''
-    createTodoView()
 })
+
+createTodoView()
